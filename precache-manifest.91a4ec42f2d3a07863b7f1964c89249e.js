5ed7157cfdc992900a3fd6fa0dc170436ecab8a1
Zedwag <zedwag@gmail.com>
2019-10-08 03:53:54 +0300

    Updates

---
+++ precache-manifest.91a4ec42f2d3a07863b7f1964c89249e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd29b0bd08db110ad748bae21960861",
+    "url": "/StarDB/index.html"
+  },
+  {
+    "revision": "7388ceb8d7a26c0f3528",
+    "url": "/StarDB/static/css/main.db887eb8.chunk.css"
+  },
+  {
+    "revision": "1edd4ede1d57a957ead8",
+    "url": "/StarDB/static/js/2.600a2e4f.chunk.js"
+  },
+  {
+    "revision": "7388ceb8d7a26c0f3528",
+    "url": "/StarDB/static/js/main.3a64341c.chunk.js"
+  },
+  {
+    "revision": "079c18ef5960b6358f31",
+    "url": "/StarDB/static/js/runtime~main.ba656614.js"
+  }
+]);
